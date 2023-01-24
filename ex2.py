3ebd8a13d89e634742986d61938905ecfb6b8762
22442952 <22442952@life.hkbu.edu.hk>
2023-01-25 07:28:23 +0800

    ex2 commit

---
+++ ex2.py
@@ -0,0 +1,8 @@
+#ex2
+# Write a function that prints all factors of the given parameter x
+def print_factor(x):
+# your code here
+ for i in range(1,x+1):
+  if x%i == 0:
+   print(i)
+print_factor(18)
